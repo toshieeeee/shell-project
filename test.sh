d71583695ea2883464c09d95f7f2645c26ddfd8d
菊池　俊樹 <toshiki_kikuchi@kikuchitoshiki-no-MacBook-Pro.local>
2015-06-28 16:08:59 +0900

    日付を付与したディレクトリのindex.htmlをSublimteで開くことに成功

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
-TODAY=`date+%Y%m%d`
+TODAY=`date +%Y%m%d`
+WORK_DIR="/Users/toshiki_kikuchi/Desktop/work/"
+cd "${WORK_DIR}"
 
+BASE_NAME=$1
+cp -r $BASE_NAME $TODAY
+mv $BASE_NAME ./bk/
+
+cd $TODAY
+open -a SublimeText2 index.html
